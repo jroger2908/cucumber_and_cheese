5dae06ec048f6f7b33bcf9c799f30993e5be8df0
JR <jroger2908@gmail.com>
2016-04-27 22:03:48 -0400

    refactored to make methods similar to gherkin

--- first_script.rb
+++ first_script.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'watir-webdriver'
-@browser = Watir::Browser.new :firefox
+
+def goto_the_puppy_adoption_site
+  @browser = Watir::Browser.new :firefox
+  @browser.goto 'http://puppies.herokuapp.com'
+end
 
 def adopt_puppy_number(num)
   @browser.button(:value => 'View Details', :index => num -1).click
@@ -11,7 +15,27 @@ def continue_adopting_puppies
   @browser.button(:value => 'Adopt Another Puppy').click
 end
 
-@browser.goto 'http://puppies.herokuapp.com'
+def checkout_with(name, address, email, pay_type)
+  @browser.button(:value => 'Complete the Adoption').click
+  @browser.text_field(:id => 'order_name').set(name)
+  @browser.text_field(:id => 'order_address').set(address)
+  @browser.text_field(:id => 'order_email').set(email)
+  @browser.select_list(:id => 'order_pay_type').select(pay_type)
+  @browser.button(:value => 'Place Order').click
+end
+
+def verify_page_contains(text)
+  fail unless @browser.text.include? text
+end
+
+def close_the_browser
+  @browser.close
+end
+
+goto_the_puppy_adoption_site
 adopt_puppy_number(1)
 continue_adopting_puppies
 adopt_puppu_number(2)
+checkout_with('Cheezy', '123 Main St', 'cheezy@foo.com', 'Check')
+verify_page_contains 'Thank you for adoption a puppy!'
+close_the_browser
