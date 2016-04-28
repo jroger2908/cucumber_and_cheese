6d2439f6ad371a3399df6f98a9d6e765def4e99f
JR <jroger2908@gmail.com>
2016-04-27 21:52:36 -0400

    refactored for readability

--- first_script.rb
+++ first_script.rb
@@ -7,7 +7,11 @@ def adopt_puppy_number(num)
   @browser.button(:value => 'Adopt Me!').click
 end
 
+def continue_adopting_puppies
+  @browser.button(:value => 'Adopt Another Puppy').click
+end
+
 @browser.goto 'http://puppies.herokuapp.com'
 adopt_puppy_number(1)
-@browser.button(:value => 'Adopt Another Puppy').click
+continue_adopting_puppies
 adopt_puppu_number(2)
