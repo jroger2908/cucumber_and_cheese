8c387280711101f363ef5685d7e03c9becc44829
JR <jroger2908@gmail.com>
2016-04-27 21:22:47 -0400

    wrote scipt to buy puppy on heroku app

--- first_script.rb
+++ first_script.rb
@@ -1,4 +1,15 @@
 require 'rubygems'
 require 'watir-webdriver'
 browser = Watir::Browser.new :firefox
-browser.goto 'http://www.apple.com'
+
+browser.goto 'http://puppies.herokuapp.com'
+browser.button(:value => 'View Details').click
+browser.button(:value => 'Adopt Me!').click
+browser.button(:value => 'Complete the Adoption').click
+browser.text_field(:id => 'order_name').set('Cheezy')
+browser.text_field(:id => 'order_address').set('123 Main St.')
+browser.text_field(:id => 'order_email').set('cheezy@foo.com')
+browser.select_list(:id => 'order_pay_type').set('Check')
+browser.button(:value => 'Place Order').click
+fail 'Browser text did not match expected value' unless browser.text.include?
+      'Thank you for adopting a puppy!'
