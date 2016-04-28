4cf1a35d8854d5bd85cd05693b5444b842cdf42c
JR <jroger2908@gmail.com>
2016-04-27 21:47:54 -0400

    refactored code to be more DRY

--- first_script.rb
+++ first_script.rb
@@ -2,14 +2,12 @@ require 'rubygems'
 require 'watir-webdriver'
 browser = Watir::Browser.new :firefox
 
+def adopt_puppy_number(browser, num)
+  browser.button(:value => 'View Details', :index => num -1).click
+  browser.button(:value => 'Adopt Me!').click
+end
+
 browser.goto 'http://puppies.herokuapp.com'
-browser.button(:value => 'View Details').click
-browser.button(:value => 'Adopt Me!').click
-browser.button(:value => 'Complete the Adoption').click
-browser.text_field(:id => 'order_name').set('Cheezy')
-browser.text_field(:id => 'order_address').set('123 Main St.')
-browser.text_field(:id => 'order_email').set('cheezy@foo.com')
-browser.select_list(:id => 'order_pay_type').select('Check')
-browser.button(:value => 'Place Order').click
-fail 'Browser text did not match expected value' unless browser.text.include?
-      'Thank you for adopting a puppy!'
+adopt_puppy_number(browser, 1)
+browser.button(:value => 'Adopt Another Puppy').click
+adopt_puppu_number(browser, 2)
