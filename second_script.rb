07676396ab1b1b9c7760421910b8c9539ee83fd4
JR <jroger2908@gmail.com>
2016-04-27 21:41:42 -0400

    added second script to select multiple puppies

--- second_script.rb
+++ second_script.rb
@@ -3,7 +3,10 @@ require 'watir-webdriver'
 browser = Watir::Browser.new :firefox
 
 browser.goto 'http://puppies.herokuapp.com'
-browser.button(:value => 'View Details').click
+browser.button(:value => 'View Details', :index => 0).click
+browser.button(:value => 'Adopt Me!').click
+browser.button(:value => 'Adopt Another Puppy').click
+browser.button(:value => 'View Details', :index => 1).click
 browser.button(:value => 'Adopt Me!').click
 browser.button(:value => 'Complete the Adoption').click
 browser.text_field(:id => 'order_name').set('Cheezy')
@@ -11,5 +14,5 @@ browser.text_field(:id => 'order_address').set('123 Main St.')
 browser.text_field(:id => 'order_email').set('cheezy@foo.com')
 browser.select_list(:id => 'order_pay_type').select('Check')
 browser.button(:value => 'Place Order').click
-fail 'Browser text did not match expected value' unless browser.text.include?
-      'Thank you for adopting a puppy!'
+
+fail unless browser.text.include? 'Thank you for adopting a puppy'
