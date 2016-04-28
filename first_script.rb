a4f64828a289253e06cc40e8c562bafef0abd34d
JR <jroger2908@gmail.com>
2016-04-27 21:49:15 -0400

    realized I could make browser and instance method

--- first_script.rb
+++ first_script.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'watir-webdriver'
-browser = Watir::Browser.new :firefox
+@browser = Watir::Browser.new :firefox
 
-def adopt_puppy_number(browser, num)
-  browser.button(:value => 'View Details', :index => num -1).click
-  browser.button(:value => 'Adopt Me!').click
+def adopt_puppy_number(num)
+  @browser.button(:value => 'View Details', :index => num -1).click
+  @browser.button(:value => 'Adopt Me!').click
 end
 
-browser.goto 'http://puppies.herokuapp.com'
-adopt_puppy_number(browser, 1)
-browser.button(:value => 'Adopt Another Puppy').click
-adopt_puppu_number(browser, 2)
+@browser.goto 'http://puppies.herokuapp.com'
+adopt_puppy_number(1)
+@browser.button(:value => 'Adopt Another Puppy').click
+adopt_puppu_number(2)
