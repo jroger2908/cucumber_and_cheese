ab8f814453e6571a431ae7739080f7b0260d1633
JR <jroger2908@gmail.com>
2016-04-28 20:44:44 -0400

    started adding step definitions

---
+++ test_puppies/features/step_definitions/adopting_puppies_steps.rb
@@ -0,0 +1,3 @@
+Given /*I am on the puppy adoption site$/ do
+  @browser.goto "http://puppies.herokuapp.com"
+end
--- test_puppies/features/support/hooks.rb
+++ test_puppies/features/support/hooks.rb
@@ -4,8 +4,6 @@ require 'watir-webdriver'
 
 Before do
   @browser = Watir::Browser.new :firefox
-
-
 end
 
 
