b69bec10433fb604c8fbabca6021a10cbda47871
JR <jroger2908@gmail.com>
2016-05-14 15:03:29 -0400

    added checkout page

--- test_puppies/features/step_definitions/adopting_puppies_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppies_steps.rb
@@ -12,27 +12,28 @@ And(/^I click the Adopt Me button$/) do
 end
 
 And(/^I click the Complete the Adoption button$/) do
-  @browser.button(:value => "Complete the Adoption").click
+  @cart.proceed_to_checkout
+  @checkout = CheckoutPage.new(@browser)
 end
 
 And(/^I enter "([^"]*)" in the name field$/) do |name|
-  @browser.text_field(:id => 'order_name').set(name)
+  @checkout.name = name
 end
 
 And(/^I enter "([^"]*)" in the address field$/) do |address|
-  @browser.text_field(:id => 'order_address').set(address)
+  @checkout.address = address
 end
 
 And(/^I enter "([^"]*)" in the email field$/) do |email|
-  @browser.text_field(:id => 'order_email').set(email)
+  @checkout.email = email
 end
 
 And(/^I select "([^"]*)" from the pay with dropdown$/) do |pay_type|
-  @browser.select(:id => 'order_pay_type').select(pay_type)
+  @checkout.pay_type = pay_type
 end
 
 And(/^I click the Place Order button$/) do
-  @browser.button(:value => 'Place Order').click
+  @checkout.place_order
 end
 
 Then(/^I should see "([^"]*)"$/) do |message|
@@ -40,7 +41,7 @@ Then(/^I should see "([^"]*)"$/) do |message|
 end
 
 And(/^I click the Adopt Another Puppy button$/) do
-  @browser.button(:value => "Adopt Another Puppy").click
+  @cart.continue_shopping
 end
 
 And(/^I click the second View Details button$/) do
