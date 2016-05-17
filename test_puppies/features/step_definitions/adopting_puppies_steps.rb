a5778df7d2d926ed1b708b19fd8388361baaa0a7
JR <jroger2908@gmail.com>
2016-05-16 20:42:21 -0400

    added more page object pages

--- test_puppies/features/step_definitions/adopting_puppies_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppies_steps.rb
@@ -1,13 +1,14 @@
 Given(/^I am on the puppy adoption site$/) do
   @browser.goto "http://puppies.herokuapp.com"
+  @home = HomePage.new(@browser)
 end
 
 When(/^I click the View Details button$/) do
-  @browser.button(:value => "View Details", :index => 0).click
+  on(HomePage).select_puppy_number 1
 end
 
 And(/^I click the Adopt Me button$/) do
-  @browser.button(:value => "Adopt Me!").click
+  @details.add_to_cart
   @cart = ShoppingCartPage.new(@browser)
 end
 
@@ -45,21 +46,19 @@ And(/^I click the Adopt Another Puppy button$/) do
 end
 
 And(/^I click the second View Details button$/) do
-  @browser.button(:value => "View Details", :index => 1).click
+  @home.select_puppy_number 2
+  @details = DetailsPage.new(@browser)
 end
 
 Then(/^I should see "([^"]*)" as the name for line item (\d+)$/) do |name, line_item|
-  # expect(cart_line_item(line_item.to_i)[1].text).to include name
   expect(@cart.name_for_line_item(line_item.to_i)).to include name
 end
 
 And(/^I should see "([^"]*)" as the subtotal for line item (\d+)$/) do |subtotal, line_item|
-  # expect(cart_line_item(line_item.to_i)[3].text).to eql subtotal
   expect(@cart.subtotal_for_line_item(line_item.to_i)).to eql subtotal
 end
 
 And(/^I should see "([^"]*)" as the cart total$/) do |total|
-  # expect(@browser.td(:class => 'total_cell').text).to eql total
   expect(@cart.cart_total).to eql total
 end
 
--- test_puppies/features/support/pages/shopping_cart_page.rb
+++ test_puppies/features/support/pages/shopping_cart_page.rb
@@ -1,11 +1,10 @@
 class ShoppingCartPage
+  include PageObject
+
   NAME_COLUMN = 1
   SUBTOTAL_COLUMN = 3
   LINES_PER_PUPPY = 6
 
-  # def initialize(browser)
-  #   @browser = browser
-  # end
 
   button(:proceed_to_checkout, :value => "Complete the Adoption")
   button(:continue_shopping, :value => "Adopt Another Puppy")
